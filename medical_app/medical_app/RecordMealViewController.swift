473b60c9a335aef7acd7fd2486fc61e0357ce226
Henry Chen <hchen113@binghamton.edu>
2019-11-08 12:31:14 -0500

    So I forgot to actually make a repository.... so yeah here

---
+++ medical_app/medical_app/RecordMealViewController.swift
@@ -0,0 +1,64 @@
+import UIKit
+
+class RecordMealViewController: UIViewController , UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    let pickerData = [String](arrayLiteral: "Breakfast", "Lunch", "Dinner", "Snack", "Small Meal")
+
+    @IBOutlet weak var meal_selection_field: UITextField!
+    @IBOutlet weak var meal_name_label: UILabel!
+    @IBOutlet weak var meal_name_textfield: UITextField!
+    @IBOutlet weak var key_ingredients_label: UILabel!
+    @IBOutlet weak var key_ingredients_textview: UITextView!
+    @IBOutlet weak var date_selection_field: UITextField!
+    
+    
+    @IBAction func date_field_edit(_ sender: UITextField) {
+        let datePickerView:UIDatePicker = UIDatePicker()
+        datePickerView.datePickerMode = UIDatePicker.Mode.date
+        sender.inputView = datePickerView
+        datePickerView.addTarget(self, action: #selector(RecordMealViewController.datePickerValueChanged), for: UIControl.Event.valueChanged)
+    }
+    
+    @objc func datePickerValueChanged(sender:UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = DateFormatter.Style.medium
+        dateFormatter.timeStyle = DateFormatter.Style.none
+        date_selection_field.text = dateFormatter.string(from: sender.date)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        key_ingredients_textview.layer.borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0).cgColor
+        key_ingredients_textview.layer.borderWidth = 1.0
+        key_ingredients_textview.layer.cornerRadius = 5
+        
+            
+        let meal_picker = UIPickerView()
+        meal_picker.delegate = self
+        meal_selection_field.inputView = meal_picker
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView( _ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+     
+    func pickerView( _ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        meal_selection_field.text = pickerData[row]
+    }
+
+}
---
+++ medical_app/medical_app/StartViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  medical_app
+//
+//  Created by Henry Chen on 11/1/19.
+//  Copyright © 2019 Henry Chen. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+    
+    struct patient_data{
+        static var name = "Henry"
+        static var counter = 0
+        static var status = "clear"
+    }
+
+    @IBOutlet weak var welcome: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var day_counter: UILabel!
+    @IBOutlet weak var status: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        let currentDate = NSDate()
+        let calendar = NSCalendar.current
+        let components = calendar.dateComponents([.hour,.minute,.second], from: currentDate as Date)
+        let hour = components.hour
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = NSLocale.current
+        dateFormatter.dateStyle = DateFormatter.Style.full
+        let convertedDate = dateFormatter.string(from: currentDate as Date)
+        
+        let day_count = patient_data.counter
+        let status_info = patient_data.status
+        
+        if (hour! < 12){
+            self.welcome.text = "Good morning, " + patient_data.name
+        }else if (hour! > 12 && hour! < 17){
+            self.welcome.text = "Good afternoon, " + patient_data.name
+        }else if (hour! > 17){
+            self.welcome.text = "Good evening, " + patient_data.name
+        }else{
+            self.welcome.text = "Hello " + patient_data.name
+        }
+        self.date.text = "Today is \n" + String(convertedDate)
+        self.day_counter.text = "Days since last flare up: " + String(day_count)
+        self.status.text = "Today's status is " + status_info
+        
+        
+    }
+
+   
+    
+    
+    
+
+    
+    
+    
+}
+
