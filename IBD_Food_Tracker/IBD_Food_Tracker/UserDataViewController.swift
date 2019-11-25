f2f32d44cb3b593033493ebeef6e66d700ab20d4
Henry Chen <hchen113@binghamton.edu>
2019-11-25 00:55:48 -0500

    user data screen

---
+++ IBD_Food_Tracker/IBD_Food_Tracker/UserDataViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserDataViewController.swift
+//  IBD_Food_Tracker
+//
+//  Created by Henry Chen on 11/24/19.
+//  Copyright © 2019 Henry Chen. All rights reserved.
+//
+
+import UIKit
+
+class UserDataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
